3061d6060266f551d018e136422bb4ab0091145c
Rafael Santos <rafinhapsantos50@gmail.com>
2024-01-05 16:29:27 -0300

    update: ajuste classe info e header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,9 +11,9 @@ import { FaUsers } from "react-icons/fa";
 import { GiStarsStack } from "react-icons/gi";
 import { Label } from "../ui/label";
 import { IoIosGitBranch } from "react-icons/io";
-import { FaGlobeAmericas } from "react-icons/fa";
-import { BsCalendar2Date } from "react-icons/bs";
-import { FaUserLarge } from "react-icons/fa6";
+import img from "./../../assets/9919.png";
+
+import InfoUser from "./info";
 
 const find = z.object({
   username: z.string(),
@@ -53,7 +53,7 @@ export default function Header() {
     <div className=" items-center justify-center">
       <Card className="mb-9 shadow-xl">
         <CardTitle className="flex justify-center my-5">GitFinder</CardTitle>
-        <CardContent className="min-h-16 flex ">
+        <CardContent className="min-h-16 flex items-center justify-center">
           <form
             className="flex items-center justify-center"
             onSubmit={handleSubmit(handleFindUsername)}
@@ -69,30 +69,31 @@ export default function Header() {
       </Card>
 
       <Card className="shadow-xl">
-        <CardContent className="mt-2">
+        <CardContent className="mt-10">
           <div className="flex">
             <div>
-              <img className="rounded-full w-28" src={data?.avatar_url} />
+              <img
+                className="rounded-full w-36 shadow-2xl border-2 border-slate-700"
+                src={data?.avatar_url || img}
+              />
             </div>
-            <div className="ml-12 flex justify-center items-center">
-              <div className="space-y-1">
-                <div className="flex space-x-1  items-center">
-                  <FaUserLarge />
-                  <Label>{data?.name}</Label>
-                </div>
 
-                <div className="flex space-x-1  items-center">
-                  <FaGlobeAmericas />
-                  <Label>{data?.location}</Label>
-                </div>
-
-                <div className="flex space-x-1  items-center">
-                  <BsCalendar2Date />
-                  <Label>{data?.created_at}</Label>
-                </div>
-              </div>
-            </div>
+            <InfoUser
+              html_url={data?.html_url || "site.com"}
+              name={data?.name || "Git Finder"}
+              company={data?.company || ""}
+              blog={data?.blog || "blog.gitfinder.com"}
+              location={data?.location || "Brusque-SC"}
+              public_repos={data?.public_repos || 2}
+              followers={data?.followers || 50}
+              created_at={data?.created_at || "2024-01-01"}
+            />
           </div>
+
+          <div className="flex  justify-center items-center my-6">
+            <Label>{data?.bio}</Label>
+          </div>
+
           <div className="flex space-x-3">
             <Card className="shadow-md">
               <CardTitle className="flex items-center justify-center mt-2">
@@ -143,6 +144,9 @@ export default function Header() {
             </Card>
           </div>
         </CardContent>
+        <div className="flex justify-center mb-10">
+          <Button>Portifolio</Button>
+        </div>
       </Card>
     </div>
   );
@@ -163,6 +167,6 @@ interface UserGitHub {
   public_gists: number;
   followers: number;
   following: number;
-  created_at: Date;
-  updated_at: Date;
+  created_at: string;
+  updated_at: string;
 }
